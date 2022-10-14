4304124fff080cf02e7967185f455623026df5c0
Prasad Surase <prasad.surase@coupa.com>
2022-10-14 12:42:31 +0530

    Added the header component with styles

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import classes from './Header.module.css';
+import mealsImage from '../../assets/images/meals.jpg';
+
+type HeaderProps = {
+  // /** CSS class(es) to apply to the badge container element */
+  // className?: string | Array<string>;
+  // /** Color to apply to the badge element */
+  // color?: "gray" | "blue" | "green" | "yellow" | "pink" | "purple";
+  // /** Size of badge component to use */
+  // size?: "small" | "medium";
+  // /** Content that should be displayed inside the Badge component */
+  // value?: any;
+  // /** Content of the badge component */
+  // children?: any;
+};
+
+const Header = (props: HeaderProps) => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>Meals Portal</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="some description goes here"/>
+      </div>
+      
+    </React.Fragment>
+  );
+};
+
+export default React.memo(Header);
